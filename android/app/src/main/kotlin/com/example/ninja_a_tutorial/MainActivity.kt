05337d68e9133fff49250e3ff4a10e08a4d4678d
GosiaKonrad <konrad.janeczek@protonmail.com>
2021-05-25 20:16:55 +0200

    1

---
+++ android/app/src/main/kotlin/com/example/ninja_a_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ninja_a_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold( // glowne opakowanie dla widgetow
+    appBar: AppBar(         // appBar to property, a wartością appBar jest widget AppBar
+      title: Text('my first App'),
+      centerTitle: true,
+      backgroundColor: Colors.red[800],
+
+
+    ),
+
+
+
+    body: Center(
+      child: Text(
+        'hello nindżasy',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+        ),
+      ),
+  ),
+
+
+
+
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+    child: Text('kliknij'),
+      backgroundColor: Colors.red[400],
+    ),
+  ),
+
+
+
+));
