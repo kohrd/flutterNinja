5eca53a13d0ec76cf64353bdd2394d4e1530bdfe
kohrd <konrad.janeczek@protonmail.com>
2021-05-28 15:59:05 +0200

    20210528_1

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,120 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold( // glowne opakowanie dla widgetow
-    appBar: AppBar(         // appBar to property, a wartością appBar jest widget AppBar
-      title: Text('my first App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[900],
-
-
-
-
-
-    ),
-
-
-
-
-
-    body: Center(
-      child: Text(
-        'hello nindżasy',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-        ),
-      ),
-  ),
-
-
-
-
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-    child: Text('kliknij'),
-      backgroundColor: Colors.red[400],
-    ),
-  ),
-
-
-
+  home: Home()
 
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build (BuildContext context){
+    return Scaffold( // glowne opakowanie dla widgetow
+      appBar: AppBar(         // appBar to property, a wartością appBar jest widget AppBar
+        title: Text('FCA Management',
+          style:TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+
+
+
+
+
+      ),
+
+
+
+
+
+      body: Center(
+        child: Text('Kyungshin app',
+          style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'
+          ),
+        ),
+      ),
+
+
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('kliknij mnie'),
+        backgroundColor: Colors.red[400],
+      ),
+    );
+
+
+
+  }
+}
+
+
+
+
+// przed 7 filmem:
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+//
+// void main() => runApp(MaterialApp(
+//   home: Scaffold( // glowne opakowanie dla widgetow
+//     appBar: AppBar(         // appBar to property, a wartością appBar jest widget AppBar
+//       title: Text('FCA Management',
+//         style:TextStyle(
+//             fontWeight: FontWeight.bold,
+//             fontSize: 30),
+//       ),
+//       centerTitle: true,
+//       backgroundColor: Colors.red[900],
+//
+//
+//
+//
+//
+//     ),
+//
+//
+//
+//
+//
+//     body: Center(
+//       child: Text('Kyungshin app',
+//         style: TextStyle(
+//             fontSize: 40.0,
+//             fontWeight: FontWeight.bold,
+//             letterSpacing: 3.0,
+//             color: Colors.grey[600],
+//             fontFamily: 'IndieFlower'
+//         ),
+//       ),
+//     ),
+//
+//
+//
+//
+//     floatingActionButton: FloatingActionButton(
+//       onPressed: (){},
+//       child: Text('kliknij'),
+//       backgroundColor: Colors.red[400],
+//     ),
+//   ),
+//
+//
+//
+//
+// ));
+//
+//
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build (BuildContext context){
+//     return Container();
+//   }
+// }
