85ed38c62167d968c1d6d3f8acfff67d598e6a1a
GosiaKonrad <konrad.janeczek@protonmail.com>
2021-05-31 07:11:40 +0200

    20210530

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home()
-
-));
-
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
-  Widget build (BuildContext context){
-    return Scaffold( // glowne opakowanie dla widgetow
-      appBar: AppBar(         // appBar to property, a wartością appBar jest widget AppBar
-        title: Text('FCA Management',
-          style:TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // glowne opakowanie dla widgetow
+      appBar: AppBar(
+        // appBar to property, a wartością appBar jest widget AppBar
+        title: Text(
+          'Proszę',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
         ),
         centerTitle: true,
-        backgroundColor: Colors.red[900],
-
-
-
-
-
+        backgroundColor: Colors.red[400],
       ),
 
 
 
-
-
-      body: Center(
-        child: Text('Kyungshin app',
-          style: TextStyle(
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-          ),
+      // body: Container(
+      //   // padding: EdgeInsets.all(20.0),
+      //   // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      //   padding: EdgeInsets.fromLTRB(11.0, 100.0, 30.0, 40.0), // inside container
+      //   margin: EdgeInsets.all(11.0),  // around container
+      //   color: Colors.grey[500],
+      //   child: Text('mozemy napisac to co chcemy albo nawet wiecej lbo jeszcze wiecej'),
+      //
+      //
+      // ),
+        body: Padding(
+          padding: EdgeInsets.all(90.0),
+          child: Text('hello')
         ),
-      ),
 
 
 
 
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text('kliknij mnie'),
-        backgroundColor: Colors.red[400],
+        onPressed: () {
+          print("Artur gra w królika bążór");
+        },
+        child: Text('kliknij'),
+        backgroundColor: Colors.purple[400],
       ),
     );
-
-
-
   }
 }
-
-
-
-
-// przed 7 filmem:
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-//
-// void main() => runApp(MaterialApp(
-//   home: Scaffold( // glowne opakowanie dla widgetow
-//     appBar: AppBar(         // appBar to property, a wartością appBar jest widget AppBar
-//       title: Text('FCA Management',
-//         style:TextStyle(
-//             fontWeight: FontWeight.bold,
-//             fontSize: 30),
-//       ),
-//       centerTitle: true,
-//       backgroundColor: Colors.red[900],
-//
-//
-//
-//
-//
-//     ),
-//
-//
-//
-//
-//
-//     body: Center(
-//       child: Text('Kyungshin app',
-//         style: TextStyle(
-//             fontSize: 40.0,
-//             fontWeight: FontWeight.bold,
-//             letterSpacing: 3.0,
-//             color: Colors.grey[600],
-//             fontFamily: 'IndieFlower'
-//         ),
-//       ),
-//     ),
-//
-//
-//
-//
-//     floatingActionButton: FloatingActionButton(
-//       onPressed: (){},
-//       child: Text('kliknij'),
-//       backgroundColor: Colors.red[400],
-//     ),
-//   ),
-//
-//
-//
-//
-// ));
-//
-//
-// class Home extends StatelessWidget {
-//   @override
-//   Widget build (BuildContext context){
-//     return Container();
-//   }
-// }
